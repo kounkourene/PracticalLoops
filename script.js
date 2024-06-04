7cfad07922db9da5ffc02fde9f8b69e869f6389f
kounkourene <kounkourene@gmail.com>
2024-06-04 08:11:04 -0700

    Added part 2: Prime Time

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Fizz Buzz
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0) {
     console.log("Fizz");
@@ -6,6 +7,36 @@ for (let i = 1; i <= 100; i++) {
   } else if (i % 3 === 0 && i % 5 === 0) {
     console.log("Fizz Buzz");
   } else {
+    console.log(i);
+  }
+}
+
+//Prime Time
+// let primeNumbers = "";
+// for (let i = 1; i <= 100; i++) {
+//   let counter = 0;
+//   for (let j = i; j >= 1; j--) {
+//     if (i % j === 0) {
+//       counter = counter + 1;
+//     }
+//   }
+//   if (counter === 2) {
+//     primeNumbers = primeNumbers + i;
+//   }
+// }
+
+// console.log(primeNumbers);
+
+for (let i = 2; i <= 100; i++) {
+  let isPrime = true;
+
+  for (let j = 2; j < i; j++) {
+    if (i % j === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  if (isPrime) {
     console.log(i);
   }
 }
